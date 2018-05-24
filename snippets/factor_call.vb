e0e14a7b4a574920d205b89846c9a79a300b9592
ebicochineal <yz94588@gmail.com>
2018-05-24 21:24:57 +0900

    Revert "Revert "syuusei""
    This reverts commit 9dffc963b0a9669279125911523907da79c6c1ee.

--- snippets/factor_call.vb
+++ snippets/factor_call.vb
@@ -7,24 +7,23 @@ Imports System.Collections.Generic
 Imports System.Diagnostics
 
 class Program
-    shared function factor (ByVal n as String) as String()
+    shared function factor (ByVal n as UInt64) as List(of UInt64)
         dim pinfo as new ProcessStartInfo()
         pinfo.FileName = "factor"
-        pinfo.Arguments = n
+        pinfo.Arguments = n.ToString()
         pinfo.CreateNoWindow = true
         pinfo.UseShellExecute = false
         pinfo.RedirectStandardOutput = true
         dim p as Process = Process.Start(pinfo)
+        dim sp() as String = p.StandardOutput.ReadToEnd().Split()
         p.WaitForExit()
-        dim s as String = p.StandardOutput.ReadToEnd()
-        dim sp() as String = s.Split()
-        dim r(sp.Length - 3) as String
-        for i as Int32 = 0 to sp.Length - 3 : r(i) = sp(i+1) : next i
+        dim r as new List(of UInt64)
+        for i as Int32 = 0 to sp.Length - 3 : r.Add(UInt64.Parse(sp(i+1))) : next i
         return r
     end function
     
     shared sub main ()
-        for each i as String in factor("999999866000004473")
+        for each i as UInt64 in factor(999999866000004473)
             Console.WriteLine(i)
         next
     end sub
