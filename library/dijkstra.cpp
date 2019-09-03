cc329be6e5bf6ec6b269c70f6f14bba49c3008d8
ebicochineal <yz94588@gmail.com>
2019-09-03 17:59:10 +0900

    add

---
+++ library/dijkstra.cpp
@@ -0,0 +1,203 @@
+#include <cmath>
+#include <string>
+#include <array>
+#include <vector>
+#include <cstdlib>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <limits>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <time.h>
+#include <sys/timeb.h>
+using namespace std;
+
+inline uint32_t xrnd() {
+    static uint32_t y = 2463534242;
+    y = y ^ (y << 13);
+    y = y ^ (y >> 17);
+    return y = y ^ (y << 5);
+}
+
+struct Edge {
+public:
+    int a, b, cost;
+    Edge () {
+        this->a = 0;
+        this->b = 0;
+        this->cost = 1000000;
+    }
+    Edge (int a, int b, int cost) {
+        this->a = a;
+        this->b = b;
+        this->cost = cost;
+    }
+    bool operator == (const Edge &t) const { return this->a == t.a && this->b == t.b; }
+};
+namespace std {
+    template <> class hash<Edge> {
+    public:
+        size_t operator()(const Edge& t) const{ return hash<int>()(t.a<<16) | hash<int>()(t.b); }
+    };
+}
+
+class GraphDijkstra {
+public:
+    class Node {
+    public:
+        int cost;
+        int prev;
+        Node (int cost, int prev) {
+            this->cost = cost;
+            this->prev = prev;
+        }
+        bool operator > (const Node &t) const { return this->cost > t.cost; }
+        bool operator < (const Node &t) const { return this->cost < t.cost; }
+    };
+    
+    int n;
+    vector< vector< pair<int, int> > > edgecost;
+    vector<int> path;
+    vector<int> rpath;
+    int pathcost;
+    
+    int tmp = -1;
+    vector<int> prevs;
+    vector<int> costs;
+    priority_queue<Node, vector<Node>, greater<Node> > que;
+    
+    GraphDijkstra (int n, vector<Edge> edges, bool undir = false) {
+        this->pathcost = 0;
+        this->path.clear();
+        this->n = n;
+        
+        this->edgecost = vector< vector< pair<int, int> > >(this->n, vector< pair<int, int> >());
+        if (undir) {// undirected
+            for (auto&& i : edges) {
+                this->edgecost[i.a].emplace_back(i.b, i.cost);
+                this->edgecost[i.b].emplace_back(i.a, i.cost);
+            }
+        } else {// directed
+            for (auto&& i : edges) {
+                this->edgecost[i.a].emplace_back(i.b, i.cost);
+            }
+        }
+    }
+    
+    GraphDijkstra (vector<Edge> edges, bool undir = false) {
+        this->pathcost = 0;
+        this->path.clear();
+        int n = 0;
+        for (auto&& i : edges) { n = max(max(i.a, i.b), n); }
+        this->n = n+1;
+        this->edgecost = vector< vector< pair<int, int> > >(this->n, vector< pair<int, int> >());
+        if (undir) {// undirected
+            for (auto&& i : edges) {
+                this->edgecost[i.a].emplace_back(i.b, i.cost);
+                this->edgecost[i.b].emplace_back(i.a, i.cost);
+            }
+        } else {// directed
+            for (auto&& i : edges) {
+                this->edgecost[i.a].emplace_back(i.b, i.cost);
+            }
+        }
+    }
+    
+    void calcPath (int start_i, int end_i) {
+        static const int limit = numeric_limits<int>::max();
+        this->path.clear();
+        this->rpath.clear();
+        if (this->tmp != start_i) {
+            this->prevs = vector<int>(this->n, -1);
+            this->costs = vector<int>(this->n, limit);
+            this->que = priority_queue<Node, vector<Node>, greater<Node> >();
+            this->costs[start_i] = 0;
+            this->que.emplace(0, start_i);
+        }
+        this->tmp = start_i;
+        
+        while (!this->que.empty()) {
+            Node t = this->que.top();
+            if (t.cost >= this->costs[end_i]) { break; }
+            this->que.pop();
+            for (auto&& i : this->edgecost[t.prev]) {
+                int cost = t.cost + i.second;
+                if (cost < this->costs[i.first]) {
+                    this->costs[i.first] = cost;
+                    this->prevs[i.first] = t.prev;
+                    this->que.emplace(cost, i.first);
+                }
+            }
+        }
+        
+        if (this->costs[end_i] < limit) {
+            this->pathcost = this->costs[end_i];
+            int e = end_i;
+            while (e > -1) {
+                this->rpath.emplace_back(e);
+                e = prevs[e];
+            }
+            this->path = this->rpath;
+            reverse(this->path.begin(), this->path.end());
+        } else {
+            this->pathcost = -1;
+        }
+    }
+};
+
+vector<Edge> gridToGraph (vector< vector<int> >& grid) {
+    static const int dx[4] = {0, 0, -1, 1};
+    static const int dy[4] = {-1, 1, 0, 0};
+    
+    vector<Edge> r;
+    int h = grid.size();
+    int w = grid[0].size();
+    for (int y = 0; y < h; ++y) {
+        for (int x = 0; x < w; ++x) {
+            int ax = x;
+            int ay = y;
+            for (int i = 0; i < 4; ++i) {
+                int bx = ax +-dx[i];
+                int by = ay +-dy[i];
+                if (bx < 0 || bx >= w || by < 0 || by >= h) { continue; }
+                r.emplace_back(Edge(ay*w+ax, by*w+bx, grid[by][bx]));
+            }
+        }
+    }
+    return r;
+}
+
+int main () {
+    vector< vector<int> > grid;
+    for (int y = 0; y < 1000; ++y) {
+        vector<int> t;
+        for (int x = 0; x < 1000; ++x) {
+            t.emplace_back(xrnd()%100);
+        }
+        grid.emplace_back(t);
+    }
+    vector<Edge> edges = gridToGraph(grid);
+    GraphDijkstra gd(edges);
+    
+    for (int i = 0; i < 8; ++i) {
+        int a = xrnd()%1000000;
+        int b = xrnd()%1000000;
+        gd.calcPath(a, b);
+        cout << a << ", " << b << ", " << gd.pathcost << endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
